45eeba53fe22b16bc83ea70e4242d0cf46e8307c
stepanow <stepanowon@gmail.com>
2023-05-19 14:38:35 +0900

    7장 불필요한 예제 변경

--- ch07/test-vue-app/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-//import App from './App2.vue'
-//import App from './App3.vue'
-
-import './assets/main.css'
-
-createApp(App).mount('#app')
