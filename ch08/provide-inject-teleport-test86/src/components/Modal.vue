9a52ab41cb0ec765dc64fc33c51f95a0e8b2c04c
Hyungseob Won <stepanowon@hotmail.com>
2023-08-18 12:58:44 +0900

    Update Modal.vue

--- ch08/provide-inject-teleport-test86/src/components/Modal.vue
+++ ch08/provide-inject-teleport-test86/src/components/Modal.vue
@@ -15,7 +15,7 @@ export default {
 <style scoped>
 .modal { display: block; position: fixed; z-index: 1; 
     left: 0; top: 0; width: 100%; height: 100%;
-    overflow: auto; background-color: rgb(0,0,0); 
+    overflow: auto; 
     background-color: rgba(0,0,0,0.4); }
 .box {   display: flex; flex-direction: column;
     align-items: center; justify-content: center;
